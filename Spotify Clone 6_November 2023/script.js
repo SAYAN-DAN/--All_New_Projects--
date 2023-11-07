2c95ad7329f9ca81c1cb6bffe162713f3c09d5d5
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-11-07 15:58:53 +0530

    Add Some Changes

---
+++ Spotify Clone 6_November 2023/script.js
@@ -0,0 +1,99 @@
+// audioElement.play();
+
+//Initialize the variables
+
+let songIndex = 0;
+let audioElement = new Audio("songs/1.mp3");
+let masterPlay = document.getElementById("masterPlay");
+let myProgressBar = document.getElementById("myProgressBar");
+let gif = document.getElementById("gif");
+let songItems = Array.from(document.querySelectorAll("#songItem"));
+
+let songs = [
+  {
+    songName: "Warriyo - Mortals [NCS Release]",
+    filePath: "songs/1.mp3",
+    coverPath: "covers/1.jpg",
+  },
+  {
+    songName: "Cielo - Huma-Huma",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/2.jpg",
+  },
+  {
+    songName: "DEAF KEV - Invincible [NCS Release]-320k",
+    filePath: "songs/3.mp3",
+    coverPath: "covers/3.jpg",
+  },
+  {
+    songName: "Different Heaven & EH!DE - My Heart [NCS Release]",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/4.jpg",
+  },
+  {
+    songName: "Janji-Heroes-Tonight-feat-Johnning-NCS-Release",
+    filePath: "songs/5.mp3",
+    coverPath: "covers/5.jpg",
+  },
+  {
+    songName: "Rabba - Salam-e-Ishq",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/6.jpg",
+  },
+  {
+    songName: "Sakhiyaan - Salam-e-Ishq",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/7.jpg",
+  },
+  {
+    songName: "Bhula Dena - Salam-e-Ishq",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/8.jpg",
+  },
+  {
+    songName: "Tumhari Kasam - Salam-e-Ishq",
+    filePath: "songs/2.mp3",
+    coverPath: "covers/9.jpg",
+  },
+  {
+    songName: "Na Jaana - Salam-e-Ishq",
+    filePath: "songs/4.mp3",
+    coverPath: "covers/10.jpg",
+  },
+];
+
+songItems.forEach((element, i) => {
+  console.log(element, i);
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
+//Handle play / pause click
+
+masterPlay.addEventListener("click", function () {
+  if (audioElement.paused || audioElement.currentTime <= 0) {
+    audioElement.play();
+    masterPlay.classList.remove("fa-circle-play");
+    masterPlay.classList.add("fa-circle-pause");
+    gif.style.opacity = 1;
+  } else {
+    audioElement.pause();
+    masterPlay.classList.remove("fa-circle-pause");
+    masterPlay.classList.add("fa-circle-play");
+    gif.style.opacity = 0;
+  }
+});
+//Listen To events
+
+audioElement.addEventListener("timeupdate", function () {
+  //    console.log("timeupdate");
+  //update seekbar
+
+  progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
+  //   console.log(progress);
+  myProgressBar.value = progress;
+});
+
+myProgressBar.addEventListener("change", function () {
+  audioElement.currentTime =
+    (myProgressBar.value * audioElement.duration) / 100;
+});
