736ca9b024e57dc2a8f51855cbdb2ac2a74a88b4
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-11-10 20:23:09 +0530

    Add Some Changes

---
+++ Tic Tac Toe Game 10_November 2023/script.js
@@ -0,0 +1,7 @@
+let music = new Audio ("music.mp3")
+let ting = new Audio ("ting.mp3")
+let gameover = new Audio ("gameover.mp3")
+
+const changeTing = function () {
+    return ting === "X"?"0" :"X"
+}
