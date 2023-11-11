94463b16e3d51ad30bdacdf391d1130c48299609
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-11-11 08:13:10 +0530

    Add Some Changes

--- Tic Tac Toe Game 10_November 2023/script.js
+++ Tic Tac Toe Game 10_November 2023/script.js
@@ -1,7 +1,45 @@
-let music = new Audio ("music.mp3")
-let ting = new Audio ("ting.mp3")
-let gameover = new Audio ("gameover.mp3")
+let music = new Audio("music.mp3");
+let Audioting = new Audio("ting.mp3");
+let gameover = new Audio("gameover.mp3");
+let ting = "X";
+
+//function to change the turn
 
 const changeTing = function () {
-    return ting === "X"?"0" :"X"
-}
+  return ting === "X" ? "0" : "X";
+};
+
+// function to check for a win
+
+const checkWin = function () {
+    let boxtexts = document.getElementsByClassName("boxtext")
+    let wins = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ]
+    wins.forEach (function (e)  {
+            if((boxtext[e[0]].innerText === boxtext[e[0]].innerText) &&(boxtext[e[0]].innerText === boxtext[e[0]].innerText)&&boxtext[e[0]].innerText !=="" ){}
+    })
+};
+
+// Game Logic
+
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach(function (element) {
+  let boxtext = element.querySelector(".boxtext");
+  element.addEventListener("click", function (e) {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = ting;
+      ting = changeTing();
+      Audioting.play();
+      checkWin();
+      document.getElementsByClassName("info")[0].innerText = "Turn for" + ting;
+    }
+  });
+});
