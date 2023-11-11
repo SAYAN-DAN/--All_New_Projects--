68dcfd687b9ff138ceeb56f5730722349a6858bc
SAYAN-DAN <sayan94.arambagh@gmail.com>
2023-11-11 21:26:20 +0530

    Add Some Changes

--- Tic Tac Toe Game 10_November 2023/script.js
+++ Tic Tac Toe Game 10_November 2023/script.js
@@ -2,6 +2,7 @@ let music = new Audio("music.mp3");
 let Audioting = new Audio("ting.mp3");
 let gameover = new Audio("gameover.mp3");
 let ting = "X";
+let gameover1 = false;
 
 //function to change the turn
 
@@ -12,24 +13,38 @@ const changeTing = function () {
 // function to check for a win
 
 const checkWin = function () {
-    let boxtexts = document.getElementsByClassName("boxtext")
-    let wins = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ]
-    wins.forEach (function (e)  {
-            if((boxtext[e[0]].innerText === boxtext[e[0]].innerText) &&(boxtext[e[0]].innerText === boxtext[e[0]].innerText)&&boxtext[e[0]].innerText !=="" ){}
-    })
+  let boxtext = document.getElementsByClassName("boxtext");
+  let wins = [
+    [0, 1, 2, 5, 5, 0],
+    [3, 4, 5, 5, 15, 0],
+    [6, 7, 8, 5, 25, 0],
+    [0, 3, 6, -5, 15, 90],
+    [1, 4, 7, 5, 15, 90],
+    [2, 5, 8, 15, 15, 90],
+    [0, 4, 8, 5, 15, 45],
+    [2, 4, 6, 5, 15, 135],
+  ];
+  wins.forEach(function (e) {
+    if (
+      boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[0]].innerText !== ""
+    ) {
+      document.querySelector(".info").innerText =
+        boxtext[e[0]].innerText + "  Won";
+      gameover1 = true;
+      document
+        .querySelector("#imgbox")
+        .getElementsByTagName("img")[0].style.width = "200px";
+      document.querySelector(
+        "#line"
+      ).style.transform = `translate(${e[3]}vw , ${e[4]}vw) rotate(${e[5]}deg)`;
+    }
+  });
 };
 
 // Game Logic
-
+// music.play()
 let boxes = document.getElementsByClassName("box");
 Array.from(boxes).forEach(function (element) {
   let boxtext = element.querySelector(".boxtext");
@@ -39,7 +54,23 @@ Array.from(boxes).forEach(function (element) {
       ting = changeTing();
       Audioting.play();
       checkWin();
-      document.getElementsByClassName("info")[0].innerText = "Turn for" + ting;
+      if (!gameover1) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn for" + ting;
+      }
     }
   });
+});
+// Add onclick listener to reset all button
+
+reset.addEventListener("click", function () {
+  let boxtexts = document.querySelectorAll(".boxtext");
+  Array.from(boxtexts).forEach(function (element) {
+    element.innerText = "";
+  });
+  ting = "X";
+  gameover1 = false;
+  document.getElementsByClassName("info")[0].innerText = "Turn for" + ting;
+  document.querySelector("#imgbox").getElementsByTagName("img")[0].style.width =
+    "0px";
 });
